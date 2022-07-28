5da5386d17a9fdfb50f94b13b830824d8cb5f5c3
Pavel Gaydos <pavel.gaydos@gmail.com>
2022-07-28 16:19:01 -0400

    Pav money class started

--- src/main/java/com/techelevator/application/Money.java
+++ src/main/java/com/techelevator/application/Money.java
@@ -1,6 +1,42 @@
 package com.techelevator.application;
 
+import com.techelevator.ui.UserInput;
+
 public class Money {
+
+    //gets current balance
+   public void feedMoney () {
+       String input = UserInput.getHomeScreenOption();
+
+      // if input == "M"
+      //  add  money to balance
+        /**/
+
+
+
+
+
+
+
+
+
+    }
+
+
+    //Add to balance
+
+    //Subtract
+
+    //pravate
+    // nickle = 0.05
+    // dime = 0.10
+    // qtr = 0.25
+    //dollar = 1.00
+
+
+    // convert to smallest amount of coins(CHANGE)
+
+    //return change
 
 
 }
