c591bdddd81509ee2e6172b8e8c59b65208acd4f
Jason Belmana <jbelmana@gmail.com>
2022-07-28 14:12:45 -0400

    Jason push 2:11

--- src/main/java/com/techelevator/VendingMachineCLI.java
+++ src/main/java/com/techelevator/VendingMachineCLI.java
@@ -3,12 +3,23 @@ package com.techelevator;
 
 import com.techelevator.application.VendingMachine;
 
+import java.util.Scanner;
+
 public class VendingMachineCLI {
 
 	public static void main(String[] args) {
 		//command line interface
 
 		VendingMachine vendingMachine = new VendingMachine();
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Welcome to Taste Elevator vending machine!");
+		System.out.println("Please select one of the following options: ");
+		System.out.println("(D) Display Vending Items");
+		System.out.println("(P) Purchase");
+		System.out.println("(E) Exit");
+		String mainOption = scanner.nextLine();
+
 		vendingMachine.run();
 	}
 }
--- src/main/java/com/techelevator/application/Audit.java
+++ src/main/java/com/techelevator/application/Audit.java
@@ -1,20 +1,41 @@
 package com.techelevator.application;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.Writer;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Audit {
 
-    public static void auditFile(){
-        
+    private static File auditFile = new File("Audit.txt");
 
-
+    public static void auditFile() {
+        if (auditFile.exists()) {
+            auditFile.delete();
+        } else {
+            try {
+                auditFile.createNewFile();
+            } catch (IOException e) {
+                System.out.println("Could not create new audit file.");
+            }
+        }
     }
-
 
 //>01/01/2016 12:00:15 PM MONEY FED:          $5.00  $10.00
 //            >01/01/2016 12:00:20 PM 7Down           B4 $10.00   $6.75
 //            >01/01/2016 12:01:25 PM SingleMint Gum  C2  $7.50   $4.40
 //            >01/01/2016 12:01:35 PM CHANGE GIVEN:       $6.45   $0.00
 //            >```
+
+    private String getCurrentTime() {
+        String date = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss a").format(new Date());
+        return date;
+    }
+
+    public String timeAudit(String event, String currentBal, String  )
+    try(Writer )
+}
+
 
 }
